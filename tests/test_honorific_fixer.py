05d051ece756537fdb994c5bd3270c255440e352
Efrain <efrondx@gmail.com>
2024-08-30 20:16:17 -0600

    Fix Tests

--- tests/test_honorific_fixer.py
+++ tests/test_honorific_fixer.py
@@ -1,11 +1,12 @@
 import unittest
 from subdeloc_tools.modules import honorific_fixer
 from tests.constants.pairsubs import RESULT
+import os
 
 class TestHonorificFixer(unittest.TestCase):
     def setUp(self):
-        self.eng_file = "./tests/files/eng.ass"
-        self.jap_file = "./tests/files/jap.ass"
+        self.eng_file = "."+os.sep+"tests"+os.sep+"files"+os.sep+"eng.ass"
+        self.jap_file = "."+os.sep+"tests"+os.sep+"files"+os.sep+"jap.ass"
 
     def test_prepare_edit_dict(self):
         result = honorific_fixer.prepare_edit_dict(RESULT)
