057c7952d9b1ca97fd76e69f05fdb31d3b3141d0
Efrain <efrondx@gmail.com>
2024-09-04 19:49:36 -0600

    Workflow dispatch

--- subdeloc_tools/modules/pairsubs.py
+++ subdeloc_tools/modules/pairsubs.py
@@ -12,13 +12,6 @@ def load_ass(file_path):
         print(f"Error loading file '{file_path}': {e}")
         return None
 
-def intersect(t1, t2):
-    if (t1[0] == t2[1] or t1[1] == t2[0]) and (t1[0] != t2[1] or t1[1] != t2[0]):
-        return False
-    if t1[0] > t2[1] or t2[0] > t1[1]:
-        return False
-    return True
-
 def sanitize_string(string):
     # Match substrings enclosed in {}
     pattern = r"\{([^{}]*)\}"
@@ -28,50 +21,51 @@ def sanitize_string(string):
     result = re.sub(r"\\.", "", result)
     return result
 
+# Intersect functions
+def check_interval_conditions(interval, other_interval):
+    # Check for full containment
+    fully_contained = (
+        (interval['start'] >= other_interval['start'] and interval['end'] <= other_interval['end']) or
+        (other_interval['start'] >= interval['start'] and other_interval['end'] <= interval['end'])
+    )
+
+    # Check if both start and end times are within the ±2 seconds margin
+    within_margin = (
+        abs(interval['start'] - other_interval['start']) <= MARGIN or 
+        abs(interval['end'] - other_interval['end']) <= MARGIN
+    )
+
+    return fully_contained, within_margin
+
+def find_matches(interval, other_set):
+    matches = []
+    for idx, other_interval in enumerate(other_set):
+        
+        fully_contained, within_margin = check_interval_conditions(interval, other_interval)
+
+        if fully_contained or within_margin:
+            matches.append(other_interval)
+
+    return matches
+
+def process_interval(interval, other_set, key_1='original'):
+    matches = find_matches(interval, other_set)
+
+    key_2 = 'reference' if key_1 == 'original' else 'original'
+
+    if matches:
+        group_start = min([interval['start']] + [b['start'] for b in matches])
+        group_end = max([interval['end']] + [b['end'] for b in matches])
+
+        return {
+            'start': group_start,
+            'end': group_end,
+            key_1: [interval],
+            key_2: matches
+        }, len(matches)
+
 def find_intersections(set_a, set_b):
     intersections = []
-
-    def check_interval_conditions(interval, other_interval):
-        # Check for full containment
-        fully_contained = (
-            (interval['start'] >= other_interval['start'] and interval['end'] <= other_interval['end']) or
-            (other_interval['start'] >= interval['start'] and other_interval['end'] <= interval['end'])
-        )
-
-        # Check if both start and end times are within the ±2 seconds margin
-        within_margin = (
-            abs(interval['start'] - other_interval['start']) <= MARGIN or 
-            abs(interval['end'] - other_interval['end']) <= MARGIN
-        )
-
-        return fully_contained, within_margin
-
-    def find_matches(interval, other_set):
-        matches = []
-        for idx, other_interval in enumerate(other_set):
-            
-            fully_contained, within_margin = check_interval_conditions(interval, other_interval)
-
-            if fully_contained or within_margin:
-                matches.append(other_interval)
-
-        return matches
-
-    def process_interval(interval, other_set, key_1='original'):
-        matches = find_matches(interval, other_set)
-
-        key_2 = 'reference' if key_1 == 'original' else 'original'
-
-        if matches:
-            group_start = min([interval['start']] + [b['start'] for b in matches])
-            group_end = max([interval['end']] + [b['end'] for b in matches])
-
-            return {
-                'start': group_start,
-                'end': group_end,
-                key_1: [interval],
-                key_2: matches
-            }, len(matches)
 
     # Pointers for iterating through set A and set B
     i, j = 0, 0
@@ -106,6 +100,7 @@ def find_intersections(set_a, set_b):
 
     return intersections
 
+# Main methods
 def group_lines_by_time(sub1, sub2):
     intervals = []
     sub_pivot = sub2
